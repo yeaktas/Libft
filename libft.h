3a0b9cd33e150496011f3efdff74dc022602c258
Yunus emre Aktas <yaktas@k2m07s03.42kocaeli.com.tr>
2022-02-12 17:59:57 +0300

    libft.h

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaktas <yaktas@student.42.tr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 13:37:27 by yaktas            #+#    #+#             */
+/*   Updated: 2022/02/10 13:51:34 by yaktas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+size_t	ft_strlen(const char *s);
+
+#endif
