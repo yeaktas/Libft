f5c2448e19d00ef2c8f0a694073655104f4576d8
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-02-13 16:13:42 +0300

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 13:37:27 by yaktas            #+#    #+#             */
-/*   Updated: 2022/02/13 15:54:42 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/02/13 16:13:20 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,5 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
 #endif
