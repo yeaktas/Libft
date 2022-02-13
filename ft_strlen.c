7e2b37eb926eba7388cd9d17d3da19f989d7b1b1
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-02-13 16:12:39 +0300

    ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 13:06:48 by yaktas            #+#    #+#             */
+/*   Updated: 2022/02/13 16:12:22 by yaktas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//girilen kelimenin harfini sayıyıyor
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*
+int	main()
+{
+	char str[] = "asd";
+	int say = ft_strlen(str);
+
+	printf("%d\n", say);
+}
+*/
