d573c73e6e836933efb413bc60c6cdfe475a0ef9
Yunus emre Aktas <yaktas@k2m02s06.42kocaeli.com.tr>
2022-02-18 17:44:49 +0300

    ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaktas <yaktas@student.42.tr>              +#+  +:+       +#+        */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 14:27:39 by yaktas            #+#    #+#             */
-/*   Updated: 2022/02/13 14:51:45 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/02/18 17:44:46 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//rakam olup olmadığını kontrol ediyor.
+//rakam olup olmadigini kontrol ediyor.
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
