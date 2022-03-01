baff6e1487dc6586217d417dcc484737d2d5a252
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-03-02 02:03:52 +0300

    ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 01:48:33 by yaktas            #+#    #+#             */
+/*   Updated: 2022/03/02 02:03:30 by yaktas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//s2 yi s1 de arar ve buldugunda devamını return eder.
+char	*ft_strnstr(const char *s1, const char *s2, size_t	n)
+{
+	size_t	i;
+	size_t	j;
+	size_t	s2len;
+
+	i = 0;
+	s2len = ft_strlen(s2);
+	if (!s2len)
+		return ((char *)s1);
+	if (n != 0)
+	{
+		while (s1[i] && i <= n - s2len)
+		{
+			j = 0;
+			while (s2[j] && s2[j] == s1[i + j])
+				j++;
+			if (j == s2len)
+				return ((char *)&s1[i]);
+			i++;
+		}
+	}
+	return (NULL);
+}
+
+/* int	main(void)
+{
+	char a[] = "merhabadünya sdfds";
+	char b[] = "ba";
+
+	printf("%s", ft_strnstr(a, b, 7));
+} */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 13:37:27 by yaktas            #+#    #+#             */
-/*   Updated: 2022/02/25 16:49:07 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/03/02 01:49:51 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -32,4 +33,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strnstr(const char *s1, const char *s2, size_t	n);
 #endif
