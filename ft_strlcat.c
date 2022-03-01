c831a497a3a51400d419f6a3d6ae6d04b3f0a19d
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-03-02 01:46:27 +0300

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,20 +6,41 @@
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 16:53:58 by yaktas            #+#    #+#             */
-/*   Updated: 2022/02/27 18:43:45 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/03/02 01:46:00 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+//deste srcyi ekler, return olarak, dest ve src uzunlugu toplanır dondurulur.
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	d;
-	size_t	s;
+	size_t	i;
+	size_t	j;
 	size_t	dlen;
 	size_t	slen;
 
-	dlen = ft_strlen(d);
-	slen = ft_strlen(d);
-	
+	dlen = ft_strlen(dst);
+	slen = ft_strlen(src);
+	i = 0;
+	j = dlen;
+	if (dlen < size - 1 && size > 0)
+	{
+		while (src[i] != '\0' && dlen + i < size - 1)
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = '\0';
+	}
+	if (dlen >= size)
+		dlen = size;
+	return (dlen + slen);
 }
+/* int	main(void)
+{
+	char a[] = "yunus";
+	char b[] = "emre";
+
+	printf("%zu", ft_strlcat(a, b, 8));
+} */
