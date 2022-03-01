28bc47d452a0295f5ecdf73a1d9348b23c675c8d
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-03-02 02:24:18 +0300

    ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,12 +6,12 @@
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 02:11:20 by yaktas            #+#    #+#             */
-/*   Updated: 2022/03/02 02:20:13 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/03/02 02:24:12 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//s dizisini 
+//s dizisini n kadar c ile doldurur. 
 void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t			i;
