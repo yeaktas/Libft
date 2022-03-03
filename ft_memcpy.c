a962cdba4dcdedb8f5a82d72a3539ad0e0520f26
yeaktas <yunusemreaktas97@gmail.com>
2022-03-03 22:32:23 +0300

    ft_isalnum

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,36 @@
 /*   By: aktas <aktas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 20:08:50 by aktas             #+#    #+#             */
-/*   Updated: 2022/03/02 20:08:52 by aktas            ###   ########.fr       */
+/*   Updated: 2022/03/03 22:29:58 by aktas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+gey yunus
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*str;
+	char	*src2;
+	size_t	i;
+
+	i = 0;
+	str = (char *)dst;
+	src2 = (char *)src;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		str[i] = src2[i];
+		i++;
+	}
+	return (dst);
+}
+
+int main()
+{
+	char	a[] = "yundfgdfgdfgus";
+	char	b[] = "erdfgdfgrr";
+
+	printf("%s", ft_memcpy(a, b, 5));
+
+}
