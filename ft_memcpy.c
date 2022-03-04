f31ec0af15768a707ebdc89df7bafdd4ff94027a
Yunus emre Aktas <yaktas@k2m04s04.42kocaeli.com.tr>
2022-03-04 12:25:53 +0300

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,39 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aktas <aktas@student.42.fr>                +#+  +:+       +#+        */
+/*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 20:08:50 by aktas             #+#    #+#             */
-/*   Updated: 2022/03/03 22:29:58 by aktas            ###   ########.fr       */
+/*   Updated: 2022/03/04 12:24:49 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-gey yunus
+//srcden deste n kadar kopyalar.
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*str;
-	char	*src2;
+	char	*dest;
+	char	*srce;
 	size_t	i;
 
 	i = 0;
-	str = (char *)dst;
-	src2 = (char *)src;
+	dest = (char *)dst;
+	srce = (char *)src;
 	if (dst == NULL && src == NULL)
 		return (NULL);
 	while (i < n)
 	{
-		str[i] = src2[i];
+		dest[i] = srce[i];
 		i++;
 	}
 	return (dst);
 }
 
-int main()
+/* int main()
 {
-	char	a[] = "yundfgdfgdfgus";
-	char	b[] = "erdfgdfgrr";
+	char	a[] = "yunushyhyhs";
+	char	b[] = "emreaktas";
 
-	printf("%s", ft_memcpy(a, b, 5));
+	printf("%s", ft_memcpy(a, b, 3));
 
 }
+ */
