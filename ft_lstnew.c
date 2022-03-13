ac9641eb0c4fcb0bf67223d39c001bb137c38d96
yeaktas <yunusemreaktas97@gmail.com>
2022-03-14 01:22:19 +0300

    libft

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,25 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/12 23:34:33 by yaktas            #+#    #+#             */
-/*   Updated: 2022/03/12 23:34:33 by yaktas           ###   ########.fr       */
+/*   Created: 2022/03/14 01:13:25 by yaktas            #+#    #+#             */
+/*   Updated: 2022/03/14 01:13:25 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-//file descriptor.
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-
-/*int main()
-{
-	char c = 'm';
-	int fd = open("test", O_WRONLY);
-	ft_putchar_fd(c, fd);
-}*/
--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <fcntl.h>
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t n);
